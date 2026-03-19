8296487cf447c6848b7d045c92cc30e4901da8a2
sarco2005 <samuelrodriguez2005ac@gmail.com>
2026-03-19 00:16:53 -0600

    Initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using TuProyecto.Models;
+
+namespace TuProyecto.Controllers
+{
+    public class AccountController : Controller
+    {
+        UsuarioDAL usuarioDAL = new UsuarioDAL();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+   
+        [HttpPost]
+        public ActionResult Login(Usuario model)
+        {
+            if (usuarioDAL.ValidarUsuario(model.Username, model.Password))
+            {
+                Session["Usuario"] = model.Username; // 🔥 guardar sesión
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ Models/UsuarioDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TuProyecto.Models
+{
+    public class UsuarioDAL
+    {
+        private string conexion = ConfigurationManager.ConnectionStrings["ConexionDB"].ConnectionString;
+
+        public bool ValidarUsuario(string username, string password)
+        {
+            bool esValido = false;
+
+            using (SqlConnection conn = new SqlConnection(conexion))
+            {
+                string query = @"SELECT COUNT(1) 
+                                 FROM Usuarios 
+                                 WHERE Username = @Username 
+                                 AND Password = @Password 
+                                 AND Activo = 1";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                conn.Open();
+
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                esValido = count > 0;
+            }
+
+            return esValido;
+        }
+    }
+}
